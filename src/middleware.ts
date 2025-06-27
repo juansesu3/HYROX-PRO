75709eb2ca2856f1b4b36cb8f1169d73886d9613
juansesu3 <juanseu3@hotmail.com>
2025-06-27 16:52:03 +0200

    first oe

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
-
-
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = request.nextUrl;
