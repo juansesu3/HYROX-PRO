d19d424a22c4d641acebc74a960dc6cee97324e0
juansesu3 <juanseu3@hotmail.com>
2025-07-04 11:09:35 +0200

    working on chat an navigation

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -14,7 +14,7 @@ export default function Navigation() {
   }
 
   return (
-    <nav className="fixed z-50 bottom-0 left-0 right-0 bg-white shadow-lg border-t border-gray-200">
+    <nav className="fixed z-50 bottom-0 left-0 right-0 bg-white shadow-lg border-t border-gray-200 pb-4">
       <div className="flex justify-around py-2">
         <NavItem href="/" pathname={pathname} icon={<GoHome size={24} />} label="Inicio" />
         <NavItem href="/profile" pathname={pathname} icon={<CiUser size={24} />} label="Perfil" />
